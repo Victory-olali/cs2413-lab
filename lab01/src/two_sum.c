3b4acdf5c35a705c2a81bf48c17e7ab02f2ad0fb
Victory Olali <volali@ttu.edu>
2026-02-05 17:38:28 -0600

    lab01: complete two_sum

--- lab01/src/two_sum.c
+++ lab01/src/two_sum.c
@@ -8,6 +8,15 @@ int two_sum(const int* nums, int n, int target, int* out_i, int* out_j) {
     //  - store indices in *out_i and *out_j
     //  - return 1 if found; otherwise return 0
 
+    for(int i = 0; i < n-1; i++ ){
+        for(int j = i + 1; j < n; j++){
+            if(nums[i] + nums[j] == target){
+                *out_i = i;
+                *out_j = j;
+                return 1;
+            }
+        }
+    }
     return 0;
 }
 
--- lab01/src/two_sum_sorted.c
+++ lab01/src/two_sum_sorted.c
@@ -2,8 +2,7 @@
 
 int two_sum_sorted(const int* nums, int n, int target, int* out_i, int* out_j) {
     if (!nums || !out_i || !out_j || n < 2) return 0;
-
-    // TODO: implement rwo sum solution for sorted arrays.
+    // TODO: implement two sum solution for sorted arrays.
 
     return 0;
 }
